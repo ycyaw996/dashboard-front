77adba836de037b75a2e42b847c3ad6cfb9c0488
wangqing <wangqing@yaosoft.cn>
2023-12-11 23:24:34 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <template>
+<el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">
+      <img
+        style="width: 100px"
+        src="https://element-plus.org/images/element-plus-logo.svg"
+        alt="Element logo"
+      />
+    </el-menu-item> 
+  </el-menu>
+
   <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
     <el-radio-button :label="false">expand</el-radio-button>
     <el-radio-button :label="true">collapse</el-radio-button>
@@ -10,30 +26,16 @@
     @open="handleOpen"
     @close="handleClose"
   >
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon><location /></el-icon>
-        <span>Navigator One</span>
-      </template>
-      <el-menu-item-group>
-        <template #title><span>Group One</span></template>
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item two</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
-      </el-menu-item-group>
-      <el-sub-menu index="1-4">
-        <template #title><span>item four</span></template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
+  <el-menu-item index="1">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>Navigator Two</template>
+    </el-menu-item>
     <el-menu-item index="2">
       <el-icon><icon-menu /></el-icon>
       <template #title>Navigator Two</template>
     </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon><document /></el-icon>
+    <el-menu-item index="3">
+      <el-icon><icon-menu /></el-icon>
       <template #title>Navigator Three</template>
     </el-menu-item>
     <el-menu-item index="4">
@@ -44,6 +46,13 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
 import { ref } from 'vue'
 import {
   Document,
@@ -65,5 +74,11 @@ const handleClose = (key: string, keyPath: string[]) => {
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+}
+.el-menu-item{
+  color: #0FCA7A !important; /* 更改为您希望的颜色 */
+}
+.flex-grow {
+  flex-grow: 1;
 }
 </style>
